ba61eeaacc8e2515a6506983ba9db0d223997baf
EvgenyMarchuk <evgenmarchuk@gmail.com>
2015-11-16 12:13:38 +0300

    Edited

--- src/main/java/helpers/TestBase.java
+++ src/main/java/helpers/TestBase.java
@@ -1,7 +1,6 @@
 package helpers;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 import static helpers.DriverSingleton.getDriver;
 
@@ -13,7 +12,7 @@ public class TestBase {
         getDriver().navigate().to(BASE_URL);
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown(){
         DriverSingleton.quit();
     }
